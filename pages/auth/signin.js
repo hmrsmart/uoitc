f2fe98ed36fcc38df2ac6e3ac0fae1f5f29e7739
hmrsmart <97602593+hmrsmart@users.noreply.github.com>
2022-01-25 12:31:05 +0300

    Update signin.js

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import { signIn, signOut, useSession } from 'next-auth/client';
-import styles from '../styles/Home.module.css';
+import styles from 'styles/Home.module.css';
 
 export default function Home() {
   const [session, loadingSession] = useSession();
